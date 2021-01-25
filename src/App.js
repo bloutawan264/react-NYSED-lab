1f8e24679377849c27b0292cdd2314e456bf0bfe
bloutawan264 <bloutawan8060@richmondhillhs.org>
2021-01-25 18:42:25 +0000

    FinalNYSED

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
-// Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+ import SchoolCard from './components/SchoolCard.js';
+ import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SchoolCard school={data[0]} />
+      <SchoolCard school={data[1]} />
+      <SchoolCard school={data[2]} />
+      <SchoolCard school={data[3]} />
+      <SchoolCard school={data[4]} />
+      <SchoolCard school={data[5]} />
+      <SchoolCard school={data[6]} />
+      <SchoolCard school={data[7]} />
+      <SchoolCard school={data[8]} />
+      <SchoolCard school={data[9]} />
+      <SchoolCard school={data[10]} />
       <div className="container">
         <div className="row">
 
